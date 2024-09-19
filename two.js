c157f39079b2b467aed4da4a2f96bb5d7227dc22
Krishna077-git <saikrishna.cs077@gmail.com>
2024-09-19 12:31:49 +0530

    checking

--- two.js
+++ two.js
@@ -1,11 +1,35 @@
-console.log("Sai Krishna")
-console.log(712003)
-console.log(true)
-console.log(false)
-console.log(1+1)
-console.log("1"+1)
-console.log(1+"2")
-console.log(true+1)
+console.log("Sai Krishna");
+console.log(typeof("sai krishna"));
+console.log(712003);
+console.log(typeof(712003));
+console.log(true);
+console.log(typeof(true));
+console.log(false);
+console.log(typeof(false));
+console.log(1+1);
+console.log("1"+1);
+console.log(1+"2");
+console.log(true+1);
+console.log["radha",25,73,0];
+console.log(typeof(0,"sai",21));
+console.log
+    {UserName:"Krishna"
+    DoB:7/1/2003
+    Place:Bangalore
+    MobileNo:9916051884};
+
+
+    
+    
+    
+
+
+    
+
+
+
+
+
 
 
 
